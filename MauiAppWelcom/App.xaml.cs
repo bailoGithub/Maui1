30aec45954189c92733687e63aa883f62e524319
Oumarou Bailo Bah <oumaroubailo.bah@monboreal.ca>
2024-11-08 16:15:22 -0500

    Mise à jour avec .gitignore et fichiers de projet

---
+++ MauiAppWelcom/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppWelcom
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
